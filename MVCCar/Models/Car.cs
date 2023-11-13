aab0277e119cb749606cddfa4dbee9243e6111ab
Tin Bašić <tin.basic22@gmail.com>
2023-11-13 21:13:39 +0100

    Add project files.

---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCCar.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }=string.Empty;
+		public string Color { get; set; }=string.Empty;
+		public int ProductionYear { get; set; }
+		public decimal Price { get; set; }
+		public string Manufacture { get; set; } = string.Empty;	
+
+
+
+
+
+	}
+}
